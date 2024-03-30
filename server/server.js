46afdc83c0138be0de757fe35de26bd093714a17
kusche12 <kylerkusche@gmail.com>
2024-03-30 15:34:02 -0700

    save git ignore and server file

---
+++ server/server.js
@@ -0,0 +1,45 @@
+// Run the server on http://localhost:4242
+// node server.js
+
+// The library needs to be configured with your account's secret key.
+// Ensure the key is kept out of any version control system you might be using.
+const stripe = require('stripe')('sk_test_...');
+const express = require('express');
+const app = express();
+
+
+// This is your Stripe CLI webhook secret for testing your endpoint locally.
+const endpointSecret = "whsec_b1597c7afc7d50f8a7963b5d141c850b03d17b6d18fde06edf6a95cd51e3665d";
+
+app.post('/webhook', express.raw({ type: 'application/json' }), (request, response) => {
+  const sig = request.headers['stripe-signature'];
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+  } catch (err) {
+    response.status(400).send(`Webhook Error: ${err.message}`);
+    return;
+  }
+
+  // Handle the event
+  switch (event.type) {
+    case 'checkout.session.completed':
+      const checkoutSessionCompleted = event.data.object;
+      // Then define and call a function to handle the event checkout.session.completed
+      break;
+    case 'customer.subscription.created':
+      const customerSubscriptionCreated = event.data.object;
+      // Then define and call a function to handle the event customer.subscription.created
+      break;
+    // ... handle other event types
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  // Return a 200 response to acknowledge receipt of the event
+  response.send();
+});
+
+app.listen(4242, () => console.log('Running on port 4242'));
