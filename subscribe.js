81ae40fbcdfb3ef91013656622ba5be9cba3babe
kusche12 <kylerkusche@gmail.com>
2024-04-07 14:48:41 -0700

    html and JS complete for subscirbe page

--- subscribe.js
+++ subscribe.js
@@ -1,21 +1,24 @@
-
 const PRO_MONTHLY_LINK = "https://buy.stripe.com/bIY2aX0vv6so4wg8ww";
-const PRO_ANNUALLY_LINK = "https://buy.stripe.com/aEUeXJa653gce6QeUV"
+const PRO_ANNUALLY_LINK = "https://buy.stripe.com/4gw7vh4LL040faUbIM"
+const PRO_PLUS_MONTHLY_LINK = "https://buy.stripe.com/aEU4j5a654kg0g0dQS";
+const PRO_PLUS_ANNUALLY_LINK = "https://buy.stripe.com/14k5n9fqp040bYI4gj";
 
 document.getElementById('priceToggle').addEventListener('click', function () {
   const proPriceDiv = document.querySelector('.planItem--pro .price');
   const proButton = document.querySelector('.planItem--pro .button');
   const proPlusPriceDiv = document.querySelector('.planItem--proPlus .price');
+  const proPlusButton = document.querySelector('.planItem--proPlus .button');
   const percentSavedDivs = document.querySelectorAll('.percentSaved');
 
   const isMonthly = proPriceDiv.textContent.includes('$4.99');
   if (isMonthly) {
     proPriceDiv.innerHTML = '$49.99<span>/ year</span>';
     proButton.href = PRO_ANNUALLY_LINK;
+    proPlusButton.href = PRO_PLUS_ANNUALLY_LINK;
   } else {
     proPriceDiv.innerHTML = '$4.99<span>/ month</span>';
-    console.log(proButton)
     proButton.href = PRO_MONTHLY_LINK;
+    proPlusButton.href = PRO_PLUS_MONTHLY_LINK;
   }
 
   if (isMonthly) {
